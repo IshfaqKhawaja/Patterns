a8fcd0da7df469012b51b5a6491413c368d38044
Ishfaq Khawaja <ishfaqkhawaja312@gmail.com>
2020-01-07 05:01:23 +0530

    Add files via upload

--- multiply without multiply operator.cpp
+++ multiply without multiply operator.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
 int main(){
-	int n,m,sum=0;
+	int n,m,sum=0,no_of_additions=0;
 	printf("Enter the value of n:\n");
 	scanf("%d",&n);
-	
+
 	printf("Enter the value of m :\n");
 	scanf("%d",&m);
-	
-	for(int i =0;i<(int)(m/2);i++)
+		int min=n,no=m;
+	if(m<n){
+	min=m;
+	no=n;
+	}
+	for(int i =0;i<(int)(min/2);i++)
 		{
-			sum=sum+n;
+			sum=sum+no;
+			no_of_additions++;
 		}
 	//printf("\n %d",sum);
 	sum+=sum;
 	if(m%2!=0)
 	sum+=n;
-	printf("The Product is: %d",sum);
-		
-		
+	printf("The Product is: %d\nTotal additions taken are %d",sum,no_of_additions);
 	
-	
-	
-	
-	
-	
-	
-	
-}
+	}
